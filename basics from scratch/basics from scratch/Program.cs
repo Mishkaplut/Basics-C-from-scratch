59fc9b0567e492fcfa1e620936117b86f73e2e28
MishkaPlut <mishka1plut@gmail.com>
2020-04-22 14:51:08 +0300

    Switch/case

---
+++ basics from scratch/basics from scratch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basics_from_scratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UserAge = Convert.ToInt32(Console.ReadLine());
+            switch(UserAge)
+            {
+                case 12:
+                    Console.WriteLine("Too young");
+                    break;
+
+                case 16:
+                    Console.WriteLine("Nice");
+                    break;
+                default:
+                    Console.WriteLine("Hmmmm");
+                    break;
+            }
+            Console.WriteLine("Goodbay");
+        }
+    }
+}
